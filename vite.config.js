b8d5fd1d33fd7f2ba40794eb767ea5c284d09c1d
Julia <juliakeo@pm.me>
2025-11-25 20:00:24 +0100

    Adds styled components

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-styled-components', { displayName: true }]]
+      }
+    })
+  ]
 })
